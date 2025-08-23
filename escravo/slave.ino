57fbeaed0d05ed520088778e335e18ef54d69fc4
JulioValente <JulioValente@users.noreply.github.com>
2025-08-22 21:17:13 -0300

    Add files via upload

---
+++ escravo/slave.ino
@@ -0,0 +1,26 @@
+#include <Serial.h> //inclui a biblioteca da comunicação serial
+
+#define LED 13  //led da placa
+#define CS 3    //pino counter strike
+
+
+char comando = 'D'; //comando ligado e desligado
+void setup() {
+  Serial.begin(9600); //inicia o serial
+
+  pinMode(LED, OUTPUT); //inicia o pino do LED como saída
+  pinMode(CS, INPUT)    //inicia o pino do counter strike como entrada
+
+  digitalWrite(LED, HIGH);  //inicializa o LED como desligado
+}
+
+void loop() {
+  if(digitalRead(CS)){ //se o slave está selecionado
+    comando = Serial.read();  //lê o comando e passa para o pino counter strike
+
+    if(comando == 'D')          //se comando desliga
+      digitalWrite(LED, LOW);   //liga o LED
+    else(comando == 'L')        //se comando liga
+      digitalWrite(LED, HIGH);  //desliga o LED
+  }  
+}
